c18d2c46cb3674f1097237edc4925078990163a1
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-12 10:49:14 -0800

    updated 3-main.c

--- tests/3-main.c
+++ tests/3-main.c
@@ -2,12 +2,15 @@
 
 /**
  * main - entry point
+ * Desciption: main file to handle 'u' 'o' 'x' 'X'
  *
  * Return: 0
  */
 
 int main(void)
 {
+	unsigned int ui;
+
 	ui = (unsigned int)INT_MAX + 1024;
 	 _printf("Unsigned:[%u]\n", ui);
 	 printf("Unsigned:[%u]\n", ui);
