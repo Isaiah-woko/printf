fa96ae16e15c42b61d6a03a6eb67914ee88846e1
be-great <begreat5333@gmail.com>
2023-11-12 18:29:59 +0200

    Delete print_pointer.c

--- print_pointer.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-/**
- * print_pointer - print the address of argumen the specifier %p
- * @args: the argument passed
- * @buffer_index: the buffer index
- * @buffer: the buffer
- * Return: number of character printed
- */
