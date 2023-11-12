54af830906051ef4ea01d43e57f2abf99ccf69d8
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-12 10:55:03 -0800

    added file for %u

---
+++ print_unsigned_int.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_unsigned_int - prints the digits of an unsigned int
+ * using the specifier %u
+ * @args: the variable number of argumets
+ * @buffer: the buffer
+ * @buffer_index: the buffer index
+ * Return: the number of digits printed
+ */
