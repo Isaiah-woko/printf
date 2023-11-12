ea403870e7e1362a1eebdd49a7be2e3eee8aabee
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-12 12:23:30 -0800

    added a new file

---
+++ print_unsigned_octal.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_unsigned_octal - a function that prints unsigned octal of a decimal
+ * @args: variable number of arguments
+ * @buffer_index: the buffer index
+ * @buffer: the buffer
+ * Return: the number of char printed
+ */
