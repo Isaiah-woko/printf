12afd629ff4e3bb46ff8ae4c89c9a58842abf52b
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-12 13:31:08 -0800

    updates were made

--- tests/3-main.c
+++ tests/3-main.c
@@ -15,14 +15,21 @@ int main(void)
 	ui = 0;
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+
 
 	ui = 12345;
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
 
 	ui = UINT_MAX; /* Maximum value for an unsigned int*/
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
 
 	ui = (unsigned int)INT_MAX + 1024;
 	 _printf("Unsigned:[%u]\n", ui);
