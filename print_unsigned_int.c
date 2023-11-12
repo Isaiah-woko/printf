a94146e6c87648aa8c712618ac83f9ffd9b20487
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-12 11:07:12 -0800

    updated %u specifier

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -8,3 +8,24 @@
  * @buffer_index: the buffer index
  * Return: the number of digits printed
  */
+
+int print_unsigned_int(va_list args, int *buffer_index, char buffer[])
+{
+	int len = 1, digits;
+	unsigned int num, highest;
+
+	num = va_arg(args, unsigned int);
+
+	/*count length of number*/
+	len = _numlen(num);
+
+	highest = _pow(10, len - 1);
+	while (highest > 0)
+	{
+		digit = (num / highest) % 10;
+		buffer_insert(digit + '0', buffer_index, buffer);
+		highest /= 10;
+	}
+
+	return (len);
+}
