d422fed731a063b145371d298eaa3ebeebc00447
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-13 10:09:46 -0800

    updates were made

--- tests/3-main.c
+++ tests/3-main.c
@@ -21,7 +21,7 @@ int main(void)
 	 printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 
 
-	ui = 12345;
+	ui = -12345;
 	_printf("\nUnsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
