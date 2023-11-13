4eb25a10c41f058227dd82cc11e23b04c7778fd0
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-13 06:31:44 -0800

    added file for %x

--- main.h
+++ main.h
@@ -24,6 +24,7 @@ int print_binary(va_list args, int *buffer_index, char buffer[]);
 int print_pointer(va_list args, int *buffer_index, char buffer[]);
 int print_unsigned_int(va_list args, int *buffer_index, char buffer[]);
 int print_unsigned_octal(va_list args, int *buffer_index, char bufffer[]);
+int print_unsigned_hexa(va_list args, int *buffer_index, char buffer[]);
 
 
 /*Helping Function*/
