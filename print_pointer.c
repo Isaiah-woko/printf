cea3d905251ad8592d2e05e85dce765f23413110
be-great <begreat5333@gmail.com>
2023-11-13 15:07:35 +0200

    Update print_pointer.c

--- print_pointer.c
+++ print_pointer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdint.h>
 
 /**
 * print_pointer - Function that print %p specifier
@@ -10,18 +11,24 @@
 int print_pointer(va_list args, int *buffer_index, char buffer[])
 {
 	int count = 0, i, hexdigit;
+
 	void *p;
+
+	uintptr_t ptr_as_int;
 
 	p = va_arg(args, void *);
 	buffer_insert('0', buffer_index, buffer);
 	buffer_insert('x', buffer_index, buffer);
 	count += 2;
-	for (i = sizeof(void *) * 2 - 5; i >= 0 ; i--)
+
+	ptr_as_int = (uintptr_t)p;
+
+	for (i = sizeof(void *) * 2 - 1; i >= 0; i--)
 	{
-		hexdigit = ((unsigned long long) p >> (i * 4)) & 0xF;
+		hexdigit = (ptr_as_int >> (i * 4)) & 0xF;
 		if (hexdigit < 10)
 			buffer_insert(hexdigit + '0', buffer_index, buffer);
-		/*digit = 11 , hex = b , ascii(a) + 1 (digit - 10)*/
+		/* digit = 11 , hex = b , ascii(a) + 1 (digit - 10) */
 		else
 			buffer_insert('a' + hexdigit - 10, buffer_index, buffer);
 		count++;
