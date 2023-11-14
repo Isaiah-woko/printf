d81650d8033ea5ead56cf28bf3ab95bddf27806e
be-great <begreat5333@gmail.com>
2023-11-14 18:21:01 +0200

    update print_unsigned_octal

--- print_unsigned_octal.c
+++ print_unsigned_octal.c
@@ -1,18 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * print_unsigned_octal - a function that prints unsigned octal of a decimal
- * @args: variable number of arguments
- * @buffer_index: the buffer index
- * @buffer: the buffer
- * Return: the number of char printed
- */
-
+* print_unsigned_octal - prints the unsigned octal representation of a decimal
+* @args: variable number of arguments
+* @buffer_index: the buffer index
+* @buffer: the buffer
+* Return: the number of characters printed
+*/
 int print_unsigned_octal(va_list args, int *buffer_index, char buffer[])
 {
 	int i = 0, j;
-	unsigned int octal;
-	int octalNumber[100];
+	unsigned int octal, temp;
+	int *octalNumber;
 	int count = 0;
 
 	octal = va_arg(args, unsigned int);
@@ -20,22 +20,33 @@ int print_unsigned_octal(va_list args, int *buffer_index, char buffer[])
 	if (octal == 0)
 	{
 		buffer_insert('0', buffer_index, buffer);
-		count++;
+		return (1);
 	}
-	/*convert unsigned int to octal*/
+	temp = octal;
+
+	while (temp > 0)
+	{
+		temp /= 8;
+		i++;
+	}
+
+	octalNumber = malloc(i * sizeof(int));
+	if (octalNumber == NULL)
+	{
+		return (0);
+	}
+	i = 0;
 	while (octal > 0)
 	{
 		octalNumber[i] = octal % 8;
 		octal /= 8;
 		i++;
 	}
-
-	/*insert octal digits as characters*/
 	for (j = i - 1; j >= 0; j--)
 	{
 		buffer_insert('0' + octalNumber[j], buffer_index, buffer);
 		count++;
 	}
-
+	free(octalNumber);
 	return (count);
 }
