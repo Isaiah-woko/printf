a0100c4ba51ec5a7306b2e2fb0b3087a2ca44931
be-great <begreat5333@gmail.com>
2023-11-14 12:09:08 +0200

    update print_rot13.c

--- print_rot13.c
+++ print_rot13.c
@@ -1,65 +1,6 @@
 #include "main.h"
 
 /**
-* __strlen - return the length of the given string
-* @s: string to check
-* Return: the length of the given string
-*/
-int __strlen(const char *s)
-{
-	int count = 0;
-
-	while (*s)
-	{
-		count++;
-		s++;
-	}
-	return (count);
-}
-
-/**
-* rot13 - shift char by 13 positions
-* @str: string to shift
-* Return: pointer of the str
-*/
-char *rot13(const char *str)
-{
-	char alph0[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-	char alph13rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	char *ptr;
-
-	int i = 0, j = 0, checker = 0;
-
-	ptr = malloc(__strlen(str) + 1);
-	if (ptr == NULL)
-		return (NULL);
-
-	j = 0;
-	while (str[j])
-	{
-		i = 0;
-		checker = 1;
-		while (alph0[i])
-		{
-			if (str[j] == alph0[i])
-			{
-				ptr[j] = alph13rot[i];
-				checker = 0;
-				break;
-			}
-			i++;
-		}
-		if (checker)
-			ptr[j] = str[j];
-		j++;
-	}
-	ptr[j] = '\0'; /* Null-terminate the string */
-	return (ptr);
-}
-
-/**
 * print_rot13 - print a string roted 13 times %R
 * @args: the argument passed
 * @buffer_index: the buffer index
@@ -68,11 +9,10 @@ char *rot13(const char *str)
 */
 int print_rot13(va_list args, int *buffer_index, char buffer[])
 {
-	int count = 0;
-
-	char *str, *nptr;
-
-	int i = 0;
+	int count = 0, i = 0, j = 0, checker = 0;
+	char alph0[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alph13rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *str;
 
 	str = va_arg(args, char*);
 	if (str == NULL)
@@ -87,13 +27,25 @@ int print_rot13(va_list args, int *buffer_index, char buffer[])
 	}
 	else
 	{
-		nptr = rot13(str);
-		for (i = 0; nptr[i] != '\0'; i++)
+		j = 0;
+		while (str[j])
 		{
-			buffer_insert(nptr[i], buffer_index, buffer);
-			count++;
+			i = 0;
+			checker = 1;
+			while (alph0[i])
+			{
+				if (str[j] == alph0[i])
+				{
+					buffer_insert(alph13rot[i], buffer_index, buffer);
+					checker = 0;
+					break;
+				}
+				i++;
+			}
+			if (checker)
+				buffer_insert(str[j], buffer_index, buffer);
+			j++;
 		}
-		free(nptr);
 	}
 	return (count);
 }
